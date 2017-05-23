3b0b660a3bbf55fcd7b12d129f30ce8694c6da8b
Jacob Manning <jacobmanning@pitt.edu>
2017-05-23 12:58:46 -0400

    Add tensorboard support, change variable organization
    Use tf.name_scope to organize variable names, show validation set
    accuracy during training, use tf.summary to write progress for
    tensorboard viewing

--- iris.py
+++ iris.py
@@ -3,10 +3,12 @@
 '''
 Machine learning for Iris dataset
   - Uses TensorFlow to create a Logistic Regression classifier
-  - Full ML pipeline with tensorflow backend
+  - Full ML pipeline with TensorFlow backend
 
 Imports raw data from csv, randomizes data, preprocesses data,
-splits data, trains model, tests model
+splits data, trains model, tests model, saves model
+
+Uses TensorBoard to visualize the results
 '''
 import sys
 import argparse
@@ -16,7 +18,7 @@ import seaborn as sns
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __author__ = 'Jacob Manning'
 __email__ = 'jacobmanning@pitt.edu'
 
@@ -96,56 +98,114 @@ def initial_plots(df):
     sns.pairplot(df, hue='label', size=2)
     plt.show()
 
+def variable_summaries(var):
+    # helper function to create summaries for tensorboard
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 def train(X_train, y_train, X_valid, y_valid, X_test, y_test, 
-            learning_rate=0.05, load=False, filename='iris_model'):
+            learning_rate=0.05, load=False, filename='iris_model', stddev=0.1):
     # parameters for size of W, b, x, y
     n_samples, n_features = X_train.shape
     # comma here necessary to unpack tuple (?,)
     n_classes, = y_train[0].shape
 
-    # variables for the weights and biases
-    W = tf.Variable(tf.random_normal([n_features, n_classes], stddev=0.1), 
-        name='weights')
-    b = tf.Variable(tf.random_normal([n_classes], stddev=0.1),
-        name='biases')
-
     # placeholders for the input -> the features and label
-    x = tf.placeholder(tf.float32, [None, n_features], name='x')
-    y_labels = tf.placeholder(tf.float32, [None, n_classes], name='y_labels')
+    with tf.name_scope('input'):
+        x = tf.placeholder(tf.float32, [None, n_features], name='x')
+        y_labels = tf.placeholder(tf.float32, [None, n_classes], name='y_labels')
+
+    # variable for the weights
+    with tf.name_scope('weights'):
+        W = tf.Variable(tf.random_normal([n_features, n_classes], stddev=stddev), 
+            name='weights')
+        variable_summaries(W)
+
+    # variable for the biases
+    with tf.name_scope('biases'):
+        b = tf.Variable(tf.random_normal([n_classes], stddev=stddev),
+            name='biases')
+        variable_summaries(b)
+
+    # node for softmax(X*W + b)
+    with tf.name_scope('softmax_Wx_plus_b'):
+        y = tf.nn.softmax(tf.matmul(x, W) + b)
+        tf.summary.histogram('activations', y)
+
+    # use tf cross entropy function and reduce mean
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_labels, logits=y))
+        tf.summary.scalar('cross_entropy', cross_entropy)
+
+    with tf.name_scope('train'):
+        with tf.name_scope('optimizer'):
+            # use gradient descent optimizer with given learning rate
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+
+        with tf.name_scope('objective'):
+            # the session will run objective -> minimize cross_entropy with g.d.
+            objective = optimizer.minimize(cross_entropy)
+
+    with tf.name_scope('evaluate'):
+        with tf.name_scope('correct_prediction'):
+            # determine if max softmax value is equal to max label value
+            correct_prediction = tf.equal(tf.argmax(y, axis=1), 
+                    tf.argmax(y_labels, axis=1))
+        
+        with tf.name_scope('accuracy'):
+            # calculate the model accuracy on the set
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    with tf.name_scope('saver'):
+        # saver node to save/restore weights/biases
+        saver = tf.train.Saver()
+
+    # merge all tf.summaries for tensorboard
+    merged = tf.summary.merge_all()
 
     # necessary node -> variable initializer
     init = tf.global_variables_initializer()
 
-    # node for softmax(X*W + b)
-    y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-    # use tf cross entropy function and reduce mean
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_labels, logits=y))
-
-    # use gradient descent optimizer with given learning rate
-    # the session will run objective -> minimize cross_entropy with g.d.
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    objective = optimizer.minimize(cross_entropy)
-
-    # determine if max value from softmax is equal to max value from label
-    correct_prediction = tf.equal(tf.argmax(y, axis=1), 
-            tf.argmax(y_labels, axis=1))
-    # calculate the model accuracy on the set
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-    # saver node to save/restore weights/biases
-    saver = tf.train.Saver()
-
     with tf.Session() as sess:
 
+        # initialize the global vars
+        sess.run(init)
+
         if not load:
-            # initialize the global vars
-            sess.run(init)
+            # writers for tensorboard
+            train_writer = tf.summary.FileWriter('tensorboard/' + 
+                filename + '/train', sess.graph)
+            test_writer = tf.summary.FileWriter('tensorboard/' + 
+                filename + '/test')
 
             # train a new model
-            for _ in range(1000):
-                sess.run(objective, feed_dict={x: X_train, y_labels: y_train})
+            for i in range(1000):
+                if i % 50 == 0:
+                    summary, acc = sess.run([merged, accuracy], 
+                        feed_dict={x: X_valid, y_labels: y_valid})
+                    test_writer.add_summary(summary, i)
+
+                    print('Validation set accuracy at step {}: {}'.format(i, acc))
+                else:
+                    summary, _ = sess.run([merged, objective],
+                        feed_dict={x: X_train, y_labels: y_train})
+                    train_writer.add_summary(summary, i)
+
+            print('\nView tensorboard with:')
+            print('tensorboard --logdir=tensorboard/' + filename, end='\n\n')
+
+            train_writer.close()
+            test_writer.close()
 
             # save the model
             save_path = saver.save(sess, 'saved_models/' + filename)
@@ -157,19 +217,20 @@ def train(X_train, y_train, X_valid, y_valid, X_test, y_test,
             saver.restore(sess, 'saved_models/' + filename)
             print('Model loaded successfully!', end='\n\n')
 
-        # print the model parameters
-        print('Weights')
-        print(sess.run(W), end='\n\n')
-        print('Biases')
-        print(sess.run(b), end='\n\n')
+            # print the model parameters
+            print('Weights')
+            print(sess.run(W), end='\n\n')
+            print('Biases')
+            print(sess.run(b), end='\n\n')
 
-        # test the model
-        print('Validation accuracy:', end=' ')
-        print(sess.run(accuracy, feed_dict={x: X_valid, y_labels: y_valid}))
-        print('Test accuracy:', end=' ')
-        print(sess.run(accuracy, feed_dict={x: X_test, y_labels: y_test}))
+            # test the model
+            print('Validation accuracy:', end=' ')
+            print(sess.run(accuracy, feed_dict={x: X_valid, y_labels: y_valid}))
+            print('Test accuracy:', end=' ')
+            print(sess.run(accuracy, feed_dict={x: X_test, y_labels: y_test}))
 
-def main(load=False, visual=False, learning_rate=0.05, filename='iris_model'):
+def main(load=False, visual=False, learning_rate=0.05, 
+    filename='iris_model', stddev=0.1):
     # load and split the data
     df = load_iris('data/iris.csv')
     train_set, valid_set, test_set = train_valid_test_split(df)
@@ -183,9 +244,16 @@ def main(load=False, visual=False, learning_rate=0.05, filename='iris_model'):
     X_valid, y_valid = label_split(valid_set)
     X_test, y_test = label_split(test_set)
 
+    # delete previous tensorboard files
+    if not load:
+        tb_dir = 'tensorboard/' + filename
+
+        if tf.gfile.Exists(tb_dir):
+            tf.gfile.DeleteRecursively(tb_dir)
+
     # run the training/testing
     train(X_train, y_train, X_valid, y_valid, X_test, y_test, load=load,
-        learning_rate=learning_rate, filename=filename)
+        learning_rate=learning_rate, filename=filename, stddev=stddev)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='iris.py',
@@ -199,8 +267,10 @@ if __name__ == '__main__':
         default=0.05, help='learning rate for GradientDescentOptimizer')
     parser.add_argument('--filename', dest='filename', type=str,
         default='iris_model', help='file to store/load model to/from')
+    parser.add_argument('--stddev', dest='stddev', type=float,
+        default=0.1, help='standard deviation for random_normal init values')
 
     args = parser.parse_args()
 
     main(load=args.load, visual=args.visual, learning_rate=args.learning_rate,
-        filename=args.filename)
+        filename=args.filename, stddev=args.stddev)
